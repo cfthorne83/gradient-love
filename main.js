906e75c758ef39e30e0ae08cc72bba6ae0ea7b6a
cthorne <cfthorne83@gmail.com>
2021-02-18 08:36:40 -0500

    work on colorpicker

--- main.js
+++ main.js
@@ -4,49 +4,61 @@ document.addEventListener("DOMContentLoaded", function(){
     canvasEl.height = 1000;
     const ctx = canvasEl.getContext('2d');
     
-    let color = 'red';
-    function draw() {
-        let j = 0;
-        ctx.clearRect(0, 0, 1000, 800);
-        for (let i = 0; i < 299; i++){
-            if (color === 'green'){
-                ctx.fillStyle = 'white';
-                ctx.fillRect(j, 0, 5, 1000);
-                color = 'black';
-            } else if (color === 'black') {
-                ctx.fillStyle = 'grey';
-                ctx.fillRect(j, 0, 10, 1000);
-                color = 'red';
-            } else {
-                ctx.fillStyle = 'black';
-                ctx.fillRect(j, 0, 20, 1000);
-                color = 'green';
-            }
-            j += 20
-            // ctx.fillStyle = 'pink';
-            // ctx.fillRect(0, 0, 1000, 1000);
-        }
-    }
+    // let color = 'red';
+    // function draw() {
+    //     let j = 0;
+    //     ctx.clearRect(0, 0, 1000, 800);
+    //     for (let i = 0; i < 299; i++){
+    //         if (color === 'green'){
+    //             ctx.fillStyle = 'white';
+    //             ctx.fillRect(j, 0, 5, 1000);
+    //             color = 'black';
+    //         } else if (color === 'black') {
+    //             ctx.fillStyle = 'grey';
+    //             ctx.fillRect(j, 0, 10, 1000);
+    //             color = 'red';
+    //         } else {
+    //             ctx.fillStyle = 'black';
+    //             ctx.fillRect(j, 0, 20, 1000);
+    //             color = 'green';
+    //         }
+    //         j += 20
+    //         ctx.fillStyle = 'pink';
+    //         ctx.fillRect(0, 0, 1000, 1000);
+    //     }
+    // }
     
-    setInterval(draw, 500);
+    // setInterval(draw, 500);
 
-    var opacity=0; 
-    var intervalID=0; 
-    window.onload=fadeout; 
-        function fadeout(){ 
-               setInterval(hide, 500); 
-        } 
-    function hide(){ 
-          var body=document.getElementById("splatter"); 
-          opacity = 
-    Number(window.getComputedStyle(body).getPropertyValue("opacity")) 
+    // var opacity=0; 
+    // var intervalID=0; 
+    // window.onload=fadeout; 
+    //     function fadeout(){ 
+    //            setInterval(hide, 500); 
+    //     } 
+    // function hide(){ 
+    //       var body=document.getElementById("splatter"); 
+    //       opacity = Number(window.getComputedStyle(body).getPropertyValue("opacity")) 
   
-            if(opacity>0){ 
-                   opacity=opacity-0.1; 
-                           body.style.opacity=opacity 
-            } 
-            else{ 
-                clearInterval(intervalID);  
-            } 
-        }  
+    //         if(opacity>0){ 
+    //                opacity=opacity-0.1; 
+    //                        body.style.opacity=opacity 
+    //         } else { 
+    //             // clearInterval(intervalID); 
+    //             opacity=1; 
+    //                 body.style.opacity=opacity  
+    //         } 
+    //     } 
+        
+    //     var colors = ["red", "orange", "white", "yellow", "green", "blue", "purple", "black"];
+    //         var currentIndex = 0;
+
+    //         setInterval(function () {
+    //             document.body.style.cssText = "background-color: " + colors[currentIndex];
+    //             currentIndex++;
+    //             if (currentIndex == undefined || currentIndex >= colors.length) {
+    //                 currentIndex = 0;
+    //             }
+    //         }, 1000);
+    
 });
---
+++ myscripts.js
@@ -0,0 +1,13 @@
+let colorInput = document.querySelector('#colorpicker')
+let hexInput = document.querySelector('#hex')
+const body = document.body
+
+colorInput.addEventListener('input', () => {
+    let color = colorInput.value;
+    hexInput.value = color;
+    // document.body.style.backgroundColor = color;
+    const div = document.createElement('div');
+    div.innerText = "hello";
+    div.style.color = color;
+    body.append(div);
+});
