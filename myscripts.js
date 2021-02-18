2e97875decbffd1b9430c61a7be1e7f60d4d481a
cthorne <cfthorne83@gmail.com>
2021-02-18 14:00:54 -0500

    work on blur

--- myscripts.js
+++ myscripts.js
@@ -5,9 +5,26 @@ const body = document.body
 colorInput.addEventListener('input', () => {
     let color = colorInput.value;
     hexInput.value = color;
+    document.body.style.backgroundColor = color;
+    // const div = document.createElement('div');
+    // div.innerText = "hello";
+    // div.style.color = color;
+    // body.append(div);
+});
+
+colorInput.addEventListener('blur', () => {
+    // let color = colorInput.value;
+    // hexInput.value = color;
     // document.body.style.backgroundColor = color;
     const div = document.createElement('div');
     div.innerText = "hello";
     div.style.color = color;
     body.append(div);
 });
+
+// function handleBlur() {
+//     const div = document.createElement('div');
+//     div.innerText = "hello";
+//     div.style.color = color;
+//     body.append(div);
+// }
