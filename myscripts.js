c4ec91c6890daaed8a60293c4610e2d2b9657172
cthorne <cfthorne83@gmail.com>
2021-02-19 10:45:27 -0500

    update stylesheets

--- myscripts.js
+++ myscripts.js
@@ -1,51 +1,51 @@
-let colorInput = document.querySelector('#colorpicker')
-let hexInput = document.querySelector('#hex')
-// const body = document.body
+// let colorInput = document.querySelector('#colorpicker')
+// let hexInput = document.querySelector('#hex')
+// // const body = document.body
 
-// let colors = [];
+// // let colors = [];
 
-// colorInput.addEventListener('blur', () => {
-//     let color = colorInput.value;
-//     colors.push(color);
-//     hexInput.value = color;
-//     // document.body.style.backgroundColor = color;
-//     const div = document.createElement('div');
-//     div.class = 'selecteCd-color'
-//     div.style.backgroundColor = color;
-//     div.style.width = '50px';
-//     div.style.height = '50px';
+// // colorInput.addEventListener('blur', () => {
+// //     let color = colorInput.value;
+// //     colors.push(color);
+// //     hexInput.value = color;
+// //     // document.body.style.backgroundColor = color;
+// //     const div = document.createElement('div');
+// //     div.class = 'selecteCd-color'
+// //     div.style.backgroundColor = color;
+// //     div.style.width = '50px';
+// //     div.style.height = '50px';
 
-//     body.append(div);
-// });
+// //     body.append(div);
+// // });
 
-let colorOne = document.querySelector('.color1'); 
-let colorTwo = document.querySelector('.color2');
-let gradient = document.querySelector('.gradient');
+// let colorOne = document.querySelector('.color1'); 
+// let colorTwo = document.querySelector('.color2');
+// let gradient = document.querySelector('.gradient');
 
-function gradientMaker(){
-    gradient.style.background = 
-    'linear-gradient(to right ' + 
-    colorOne.value + ',' + 
-    colorTwo.value + ')';
-}
+// function gradientMaker(){
+//     gradient.style.background = 
+//     'linear-gradient(to right ' + 
+//     colorOne.value + ',' + 
+//     colorTwo.value + ')';
+// }
 
-// colorOne.addEventListener('input', gradientMaker);
-// colorTwo.addEventListener('input', gradientMaker);
+// // colorOne.addEventListener('input', gradientMaker);
+// // colorTwo.addEventListener('input', gradientMaker);
 
-var color1 = document.querySelector(".color1"); 
-var color2 = document.querySelector(".color2"); 
-var body = document.querySelector(".change"); 
+// var color1 = document.querySelector(".color1"); 
+// var color2 = document.querySelector(".color2"); 
+// var body = document.querySelector(".change"); 
       
-    // Changing color for the gradient 
-    function changeGradient() { 
-        body.style.background = 
-            "linear-gradient(to right, " 
-                + color1.value + ", " 
-                + color2.value + ")"; 
+//     // Changing color for the gradient 
+//     function changeGradient() { 
+//         body.style.background = 
+//             "linear-gradient(to right, " 
+//                 + color1.value + ", " 
+//                 + color2.value + ")"; 
   
-        // css.textContent = body.style.background + ";"; 
-    } 
+//         // css.textContent = body.style.background + ";"; 
+//     } 
   
-    color1.addEventListener("input", changeGradient); 
+//     color1.addEventListener("input", changeGradient); 
   
-    color2.addEventListener("input", changeGradient);
+//     color2.addEventListener("input", changeGradient);
