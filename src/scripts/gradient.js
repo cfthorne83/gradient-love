f8ce01d939117acb267c2dda2a9fa466ae969c03
cthorne <cfthorne83@gmail.com>
2021-02-21 17:29:55 -0500

    add angle slider

--- src/scripts/gradient.js
+++ src/scripts/gradient.js
@@ -1,23 +1,24 @@
+let body = document.querySelector("body"); 
 let color1 = document.querySelector(".color1"); 
 let color2 = document.querySelector(".color2"); 
 let color3 = document.querySelector(".color3"); 
 let color4 = document.querySelector(".color4"); 
 let color5 = document.querySelector(".color5"); 
-let sky = document.querySelector(".sky"); 
-let sliderNum = document.querySelector("#sliderStatus")
+let speedStatus = document.querySelector("#speedStatus")
+let angleStatus = document.querySelector("#angleStatus")
   
-// Changing color for the gradient 
+
 function changeGradient() { 
-    sky.style.background = 
+    body.style.background = 
         "linear-gradient(" 
+        + angleStatus.innerText + "deg,"
         + color1.value + ", " 
         + color2.value + ", " 
         + color3.value + ", " 
         + color4.value + ", " 
         + color5.value + ")"; 
-    sky.style.backgroundSize = "400% 400%";
-    sky.style.animation = "gradient-animation 5s ease infinite";
-    sky.style.animation = "gradient-animation " + sliderNum.innerText + "s ease infinite";
+    body.style.backgroundSize = "400% 400%";
+    body.style.animation = "gradient-animation " + speedStatus.innerText + "s ease infinite";
 } 
 
 color1.addEventListener("input", changeGradient); 
@@ -27,10 +28,32 @@ color4.addEventListener("input", changeGradient);
 color5.addEventListener("input", changeGradient); 
 
 
-function sliderChange(val) {
-    sliderNum.innerText = val;
-    sky.style.animation = "gradient-animation " + val + "s ease infinite";
+function speedChange(val) {
+    speedStatus.innerText = val;
+    body.style.animation = "gradient-animation " + val + "s ease infinite";
 }
+
+function angleChange(val) {
+    angleStatus.innerText = val;
+    body.style.background = 
+        "linear-gradient(" 
+        + val + "deg,"
+        + color1.value + ", " 
+        + color2.value + ", " 
+        + color3.value + ", " 
+        + color4.value + ", " 
+        + color5.value + ")"; 
+    body.style.backgroundSize = "400% 400%";
+    body.style.animation = "gradient-animation " + speedStatus.innerText + "s ease infinite";
+}
+
+
+
+
+
+
+
+
 
 
 // var colors = ["red", "orange", "white", "yellow", "green", "blue", "purple", "black"];
