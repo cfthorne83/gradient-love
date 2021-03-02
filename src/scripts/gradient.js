3e6ad5cba3b350343fbd9b87960227c2cb8f07a0
cthorne <cfthorne83@gmail.com>
2021-03-02 08:30:48 -0500

    start scroll function

--- src/scripts/gradient.js
+++ src/scripts/gradient.js
@@ -1,12 +1,13 @@
 let body = document.querySelector("body"); 
+
 let color1 = document.querySelector(".color1"); 
 let color2 = document.querySelector(".color2"); 
 let color3 = document.querySelector(".color3"); 
 let color4 = document.querySelector(".color4"); 
-let color5 = document.querySelector(".color5"); 
+let color5 = document.querySelector(".color5");
+
 let speedStatus = document.querySelector("#speedStatus")
-let angleStatus = document.querySelector("#angleStatus")
-  
+let angleStatus = document.querySelector("#angleStatus")  
 
 function changeGradient() { 
     body.style.background = 
@@ -44,9 +45,46 @@ function angleChange(val) {
         + color4.value + ", " 
         + color5.value + ")"; 
     body.style.backgroundSize = "400% 400%";
-    body.style.animation = "gradient-animation " + speedStatus.innerText + "s ease infinite";
+    body.style.animation = "top-bottom " + speedStatus.innerText + "s ease infinite";
 }
 
+
+// scroll angle
+
+let startLeft = 50;
+let startTop = 0;
+let midLeft = 50;
+let midTop = 100;
+
+let scrollStatus = document.querySelector("#scroll-status")
+let scrollStart = document.querySelector("#scroll-start")
+let scrollMid = document.querySelector("#scroll-mid")
+let scrollEnd = document.querySelector("#scroll-end")
+
+function scrollChange(val) {
+    scrollStatus.innerText = val;
+    
+    if (val > 0 && val <= 50) {
+        startLeft = 50 + parseInt(val);
+        startTop = 0;
+        midLeft = 50 - parseInt(val);
+        midTop = 100;
+    } else if (val > 50 && val <= 100) {
+        startLeft = 0;
+        startTop = 100 - (parseInt(val) - 50);
+        midLeft = 100;
+        midTop = parseInt(val) - 50;
+    } else if (val > 100 && val <= 150) {
+        startLeft = 0;
+        startTop = 100 - (parseInt(val) - 50);
+        midLeft = 100;
+        midTop = parseInt(val) - 50;
+    }
+
+    scrollStart.innerText = startLeft + " " + startTop;
+    scrollMid.innerText = midLeft + " " + midTop;
+    scrollEnd.innerText = startLeft + " " + startTop;
+}
 
 
 
