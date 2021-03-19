f696cca70edf5bb6ad91e1b783510e36e5f6e489
cthorne <cfthorne83@gmail.com>
2021-03-19 17:06:34 -0400

    shorten h2

--- src/scripts/gradient.js
+++ src/scripts/gradient.js
@@ -616,6 +616,7 @@ let scrollAngle = document.querySelector(".scroll-angle");
 scrollAngle.oninput = scrollChange;
 
 function scrollChange(e) {
+let scrollAngle = document.querySelector(".scroll-angle");
     
     const val = e.target.value;
 
